f7daaa8b17313e3976a1533f49b0883dc9c2f607
leandrocombinato <leandro.combinato@quantumit.com.br>
2018-01-16 22:46:20 -0200

    Adicionar mensagem no Config

--- src/test/java/nl/oa/demo/cucumber/CucumberConfiguration.java
+++ src/test/java/nl/oa/demo/cucumber/CucumberConfiguration.java
@@ -6,5 +6,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "nl.oa.demo.cucumber.service")
 public class CucumberConfiguration {
-
+	//Configure seus Beans aqui ...	
 }
